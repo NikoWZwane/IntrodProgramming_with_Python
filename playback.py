61427e60313535d6811d752273669d01915c3371
NikoWZwane <williezn92@gmail.com>
2025-07-14 12:12:50 +0200

    playback

--- playback.py
+++ playback.py
@@ -1,3 +1,4 @@
-word_to_repl = "the c50 university"
+word_to_repl = input("Enter sentence ")
+#word_to_repl = "the c50 university"
 word_to_repl = word_to_repl.replace(" ", "...")
 print(word_to_repl)
