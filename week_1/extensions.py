c4c2c775fb5b27f7ddf374f565bce5ff22ecadfa
NikoWZwane <williezn92@gmail.com>
2025-07-23 15:33:13 +0200

    file extensions

--- week_1/extensions.py
+++ week_1/extensions.py
@@ -1,3 +1,18 @@
-file_name = input("Enter file")
+file_name = input("Enter file ")
 if file_name.endswith("gif"):
-    print(f"image")
+    print(f"image/gif")
+elif file_name.endswith(".jpg"):
+    print(f"image/jpg")
+elif file_name.endswith(".jpeg"):
+    print(f"image/{file_name}")
+elif file_name.endswith(".png"):
+    print(f"image/png")
+elif file_name.endswith(".pdf"):
+    print(f"image/pdf")
+elif file_name.endswith(".txt"):
+    print(f"image/txt")
+elif file_name.endswith(".zip"):
+    print("image/zip")
+else:
+    print("Application/octect-stream")
+
