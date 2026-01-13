183af75ed46b221786a0947d56fae88f279786e6
NikoWZwane <williezn92@gmail.com>
2026-01-13 15:44:06 +0200

    number of calories

---
+++ week_2/nutrition.py
@@ -0,0 +1,7 @@
+fruit = input("Item:")
+if fruit == "Apple":
+    print(f"Item:{fruit}\n calories: 110" )
+elif fruit == "Banana":
+    print(f"Item:{fruit}\n calories: 10" )
+else:
+    print("not a fruit")
---
+++ week_2/plates.py
@@ -0,0 +1,6 @@
+plate = input("Enter text: ")
+if len(plate) > 6:
+    print("invalid")
+elif plate.startswith("0")
+    print("Invalid")
+s
