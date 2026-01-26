7de3465784777542bf27928f05e32d547a985515
NikoWZwane <williezn92@gmail.com>
2026-01-26 11:45:17 +0200

    adding IF statement to slove problem

--- week_2/nutrition.py
+++ week_2/nutrition.py
@@ -1,7 +1,24 @@
 fruit = input("Item:")
 if fruit == "Apple":
-    print(f"Item:{fruit}\n calories: 110" )
+    print(f"Item:{fruit}\n calories: 130" )
 elif fruit == "Banana":
-    print(f"Item:{fruit}\n calories: 10" )
+    print(f"Item:{fruit}\n calories: 110" )
+elif fruit == "Grapefruit":
+    print(f"Item:{fruit}\n calories: 60" )
+elif fruit == "Honeydew Melon":
+    print(f"Item:{fruit}\n calories: 50" )
+elif fruit == "Lemon":
+    print(f"Item:{fruit}\n calories: 15" )
+elif fruit == "Nectarine":
+    print(f"Item:{fruit}\n calories: 60" )
+elif fruit == "Peach":
+    print(f"Item:{fruit}\n calories: 60" )
+elif fruit == "Pineapple":
+    print(f"Item:{fruit}\n calories:50 " )
+elif fruit == "Strawberries":
+    print(f"Item:{fruit}\n calories:50 " )
+elif fruit == "Tangarine":
+    print(f"Item:{fruit}\n calories: 50" )
 else:
+
     print("not a fruit")
